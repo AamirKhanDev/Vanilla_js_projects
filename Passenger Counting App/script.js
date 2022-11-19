6835063a247ba4bcfe1de424968c9748f5c00bce
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-11-19 20:06:40 +0000

    added method to save attendance numbers + also reset count to 0 after clicking save

--- Passenger Counting App/script.js
+++ Passenger Counting App/script.js
@@ -1,7 +1,15 @@
 const countEl = document.getElementById('count-el');
+const saveEl = document.getElementById('save-el')
 let count = 0;
 
 function incrementCounter () {
-  count = count + 1
-  countEl.innerText = count
+  count += 1
+  countEl.textContent = count
+}
+
+function save () {
+  let countStr = count + "  -  "
+  saveEl.textContent += countStr
+  countEl.textContent = 0
+  count = 0
 }
