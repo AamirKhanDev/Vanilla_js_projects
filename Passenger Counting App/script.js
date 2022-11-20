1376e412f037915cddc608b4f645da5d5ff26d23
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-11-20 20:31:39 +0000

    Setup basketball score board template

--- Passenger Counting App/script.js
+++ Passenger Counting App/script.js
@@ -1,5 +1,5 @@
 const countEl = document.getElementById('count-el');
-const saveEl = document.getElementById('save-el')
+const saveEl = document.getElementById('save-el');
 let count = 0;
 
 function incrementCounter () {
